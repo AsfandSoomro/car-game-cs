1e2cbe64381680f282a6f7fe3a105621aad0572b
Asfand Soomro <77663955+AsfandSoomro@users.noreply.github.com>
2023-02-07 22:02:45 +0500

    initial, game background and car images placed, car moveable

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace CarGame_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbUserCar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUserCar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(766, 716);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbUserCar
+            // 
+            this.pbUserCar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(75)))), ((int)(((byte)(76)))));
+            this.pbUserCar.Image = ((System.Drawing.Image)(resources.GetObject("pbUserCar.Image")));
+            this.pbUserCar.Location = new System.Drawing.Point(239, 554);
+            this.pbUserCar.Name = "pbUserCar";
+            this.pbUserCar.Size = new System.Drawing.Size(76, 114);
+            this.pbUserCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbUserCar.TabIndex = 1;
+            this.pbUserCar.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(765, 713);
+            this.Controls.Add(this.pbUserCar);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUserCar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbUserCar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarGame_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int moveSpeed = 10;
+            // To move the car right when the user presses the right arrow key
+            if (e.KeyCode == Keys.Right) {
+                Point currentLocation = pbUserCar.Location;
+                Point updatedLocation = new Point(currentLocation.X + moveSpeed, currentLocation.Y);
+                pbUserCar.Location = updatedLocation;
+            }
+            // To move the car left when the user presses the left arrow key
+            if (e.KeyCode == Keys.Left)
+            {
+                Point currentLocation = pbUserCar.Location;
+                Point updatedLocation = new Point(currentLocation.X - moveSpeed, currentLocation.Y);
+                pbUserCar.Location = updatedLocation;
+            }
+        }
+    }
+}
