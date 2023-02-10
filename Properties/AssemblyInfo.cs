5d68b101d1aa28212c801a281ca78bf832cd9f46
Asfand Soomro <77663955+AsfandSoomro@users.noreply.github.com>
2023-02-11 01:40:00 +0500

    v1.0.0.0 release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CarGame_CS")]
+[assembly: AssemblyTitle("CarGame")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CarGame_CS")]
+[assembly: AssemblyProduct("CarGame")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
